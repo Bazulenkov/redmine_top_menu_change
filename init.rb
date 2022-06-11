161e2f8eea0c57441c7b4ba3982e9293e6250d13
Bazulenkov <anton@bazul.ru>
2022-06-12 01:34:59 +0400

    .

---
+++ init.rb
@@ -0,0 +1,12 @@
+require 'redmine'
+
+Redmine::Plugin.register :top_menu_add_link do
+  name 'Top Menu Add Link plugin'
+  author 'Anton Bazulenkov'
+  description 'This is a plugin for Redmine. Adds some items on top menu.'
+  version '1.0.0'
+  url 'https://github.com/Bazulenkov/redmine_top_menu_change'
+  author_url 'https://github.com/Bazulenkov'
+
+  menu :top_menu, :issues, { :controller => 'issues', :action => 'index', :project_id => nil}, :last => true,  :caption => :label_issue_plural
+end
